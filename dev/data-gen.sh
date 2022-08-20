ca64ba3caa1df9da7ef8e0c57e04c022bb3b6952
jiachuan.zhu <chncaesar@gmail.com>
2022-08-20 17:09:20 +0800

    Add data-gen k8s image

--- dev/data-gen.sh
+++ dev/data-gen.sh
@@ -10,8 +10,6 @@ spark-submit --master local \
 --conf spark.executor.extraClassPath=/work/server/byzer-benchmark/libs/hadoop-aws-3.1.1.jar:/work/server/byzer-benchmark/libs/aws-java-sdk-bundle-1.11.271.jar \
 --conf spark.driver.extraClassPath=/work/server/byzer-benchmark/libs/hadoop-aws-3.1.1.jar:/work/server/byzer-benchmark/libs/aws-java-sdk-bundle-1.11.271.jar \
 --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
---conf spark.hadoop.fs.s3a.access.key=AKIAQGCLNPTAX5HKPT4Y \
---conf spark.hadoop.fs.s3a.secret.key=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 \
 /work/server/byzer-benchmark/libs/byzer-benchmark-0.0.1.jar \
 s3a://zjc-2/tpcds_parquet_nojuicefs_1gb \
 /work/server/byzer-benchmark/tpcds-kit/ \
